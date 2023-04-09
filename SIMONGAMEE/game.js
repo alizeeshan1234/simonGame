6418d3f762ee8af983b8dea93bb09d9ebfd4809f
alizeeshan1234 <93642091+alizeeshan1234@users.noreply.github.com>
2023-04-10 00:16:05 +0530

    Update game.js

--- SIMONGAMEE/game.js
+++ SIMONGAMEE/game.js
@@ -1,11 +1,11 @@
 
-var buttonColours = ["red", "blue", "green", "yellow"];
+let buttonColours = ["red", "blue", "green", "yellow"];
 
-var gamePattern = [];
-var userClickedPattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
 
-var started = false;
-var level = 0;
+let started = false;
+let level = 0;
 
 $(document).keypress(function () {
     if (!started) {
@@ -17,7 +17,7 @@ $(document).keypress(function () {
 
 $(".btn").click(function () {
 
-    var userChosenColour = $(this).attr("id");
+    let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
@@ -64,8 +64,8 @@ function nextSequence() {
     level++;
     $("#level-title").text("Level " + level);
 
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -73,7 +73,7 @@ function nextSequence() {
 }
 
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
